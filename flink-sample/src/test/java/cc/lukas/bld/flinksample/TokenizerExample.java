a8ea08c468a197e8548b4846b918f9449402c604
Lukas Streiter <buffaloluk7@gmail.com>
2016-06-05 00:32:11 +0200

    Gardening

--- flink-sample/src/test/java/cc/lukas/bld/flinksample/TokenizerExample.java
+++ flink-sample/src/test/java/cc/lukas/bld/flinksample/TokenizerExample.java
@@ -17,7 +17,6 @@ public class TokenizerExample {
 
     @Test
     public void runTokenizerExample() throws Exception {
-
         final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
         final DataSet<String> sampleString = env.fromElements("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus scelerisque, metus et commodo pharetra, diam mi dapibus purus, interdum accumsan mi turpis eget elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse potenti. Cras eros eros, viverra id velit sed, tempus mollis quam. Vivamus maximus tristique laoreet. Vestibulum tempus fringilla metus. Vestibulum viverra nibh vitae neque hendrerit malesuada at vitae ante. Phasellus lobortis non diam sed commodo. Pellentesque facilisis massa a erat vulputate, id ornare felis malesuada. Nam nec dignissim est. Nulla sed pellentesque risus.");
 
